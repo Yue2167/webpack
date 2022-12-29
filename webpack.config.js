08be79908d31264aca7602ba4088b52428944efa
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2022-12-29 18:28:21 +0800

    第一次更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',//这个./src/index.js是自己设置的
     output: {
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin()]
 };
