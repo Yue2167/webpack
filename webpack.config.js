bb28536de0ce3853917096901aa60e6749203a1b
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2022-12-30 00:16:13 +0800

    update2

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     entry: './src/index.js',//这个./src/index.js是自己设置的
     output: {
-        filename: '[name].[contenthash].js'
+        filename: 'index.[contenthash].js'
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Yue',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
