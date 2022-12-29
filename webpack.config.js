89cb8e0060935b6b84ce2c8f07b4350688e73ae8
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2022-12-29 17:01:37 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',//这个./src/index.js是自己设置的
+    output: {
+        filename: '[name].[contenthash].js'
+    }
+};
