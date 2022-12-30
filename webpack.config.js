4c227ba6cd5547b28293fca8e3d34ed1bdcdc561
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2022-12-30 16:47:03 +0800

    修复了bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    entry: './src/index.js',//这个./src/index.js是自己设置的
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Yue',
-        template: 'src/assets/index.html'
-    })],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  entry: "./src/index.js", //这个./src/index.js是自己设置的
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Yue",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
