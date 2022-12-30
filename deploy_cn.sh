bf60f761bffcaf4c080aae86c451d0a09ee90b85
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2022-12-31 01:35:53 +0800

    update

--- deploy_cn.sh
+++ deploy_cn.sh
@@ -1,9 +1,9 @@
 yarn build &&
 git checkout gh-pages &&
-rm -rf*.html*.js*.css*.png &&
+rm- rf *.html*.js*.css*.png &&
 mv dist/* ./ &&
-rm -rf dist;
+rm- rf dist;
 git add . &&
 git commit -m "update" &&
 git push gitee gh-pages:master &&
-git checkout main
+git checkout -
